085b559551872ac7bca2ac527920565b78b872cd
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-05 22:37:51 +0200

    create contact controller and add first endpoint with dummy return

---
+++ src/main/java/com/eazybytes/eazybank/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.eazybytes.eazybank.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("contact")
+    public String saveContactInQueryDetails(){
+        return "InQuery details are saved to the DB";
+    }
+}
