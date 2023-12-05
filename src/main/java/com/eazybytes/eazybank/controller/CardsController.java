c08f2458cddca94b18fb06cf27d25ac7b92afdfc
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-05 22:34:35 +0200

    add first end point in the cards controller with dummy return

--- src/main/java/com/eazybytes/eazybank/controller/CardsController.java
+++ src/main/java/com/eazybytes/eazybank/controller/CardsController.java
@@ -1,7 +1,13 @@
 package com.eazybytes.eazybank.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CardsController {
+
+    @GetMapping("myCards")
+    public String getCardsDetails(){
+        return "Here are cards details from the DB";
+    }
 }
