ded514c110803fd0238ae4122e8be4181a2223f9
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-09 08:43:25 +0200

    add reposotiry for Notice entity

---
+++ src/main/java/com/eazybytes/eazybank/repositry/NoticeRepository.java
@@ -0,0 +1,16 @@
+package com.eazybytes.eazybank.repositry;
+
+import com.eazybytes.eazybank.model.Notice;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoticeRepository extends CrudRepository<Notice, Integer> {
+    @Query(
+            value = "from Notice n where CURDATE() BETWEEN noticeBegDt AND noticeEndDt"
+    )
+    List<Notice> findAllActiveNotices();
+}
