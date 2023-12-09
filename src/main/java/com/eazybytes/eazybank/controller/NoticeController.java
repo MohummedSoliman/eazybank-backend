1572cf1172ff8c9f4c89dbe4a419971956f30399
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-09 10:00:04 +0200

    change return type for method to return all notice from DB instead of static string

--- src/main/java/com/eazybytes/eazybank/controller/NoticeController.java
+++ src/main/java/com/eazybytes/eazybank/controller/NoticeController.java
@@ -1,13 +1,21 @@
 package com.eazybytes.eazybank.controller;
 
+import com.eazybytes.eazybank.model.Notice;
+import com.eazybytes.eazybank.repositry.NoticeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class NoticeController {
 
+    @Autowired
+    private NoticeRepository noticeRepository;
+
     @GetMapping("notices")
-    public String getNotices(){
-        return "Here are the notices details from the DB";
+    public List<Notice> getNotices(){
+        return noticeRepository.findAllActiveNotices();
     }
 }
