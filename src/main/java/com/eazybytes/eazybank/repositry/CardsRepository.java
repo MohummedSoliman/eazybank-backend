48d8a0edaaf1a8efc9a581d3373fae7010746d68
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-09 07:57:29 +0200

    add repository for cards entity

---
+++ src/main/java/com/eazybytes/eazybank/repositry/CardsRepository.java
@@ -0,0 +1,12 @@
+package com.eazybytes.eazybank.repositry;
+
+import com.eazybytes.eazybank.model.Cards;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CardsRepository extends CrudRepository<Cards, Integer> {
+    List<Cards> findByCustomerId(int customerId);
+}
