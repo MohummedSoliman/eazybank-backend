48f53771c519e5a5e61baabc777eef0e8b01de61
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-09 11:07:21 +0200

    add fucntion to create contact random number and save the contact entity

--- src/main/java/com/eazybytes/eazybank/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazybank/controller/ContactController.java
@@ -1,13 +1,30 @@
 package com.eazybytes.eazybank.controller;
 
+import com.eazybytes.eazybank.model.Contact;
+import com.eazybytes.eazybank.repositry.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
 
 @RestController
 public class ContactController {
 
+    @Autowired
+    private ContactRepository contactRepository;
     @GetMapping("contact")
-    public String saveContactInQueryDetails(){
-        return "InQuery details are saved to the DB";
+    public Contact saveContactInQueryDetails(@RequestBody Contact contact){
+        contact.setContactId(getServiceRequestNumner());
+        contact.setCreateDt(new Date(System.currentTimeMillis()));
+        return contactRepository.save(contact);
+    }
+
+    private String getServiceRequestNumner(){
+        Random random = new Random();
+        int ranNum  = random.nextInt(9999999 - 9999) + 9999;
+        return "SR" + ranNum;
     }
 }
