7d987fa731a30de977fc40efdab53342d668f049
Mohamed Ibrahim <mosuliman92@gmail.com>
2023-12-09 21:47:20 +0200

    add custom filter to chain before basci filter chain

--- src/main/java/com/eazybytes/eazybank/config/ProjectSecurityConfig.java
+++ src/main/java/com/eazybytes/eazybank/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.eazybytes.eazybank.config;
 
+import com.eazybytes.eazybank.filter.RequestValidationBeforeFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -17,10 +21,14 @@ import java.util.Collections;
 @Configuration
 public class ProjectSecurityConfig {
 
+    CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+
     @Bean
     SecurityFilterChain defaultSecurityFIlterChain(HttpSecurity http) throws Exception {
         http.csrf(csrf -> {
-                    csrf.ignoringRequestMatchers("/contact", "register");
+            csrf.csrfTokenRequestHandler(requestHandler).
+                    ignoringRequestMatchers("/contact", "/register")
+                    .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
                 })
                 .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
@@ -34,9 +42,14 @@ public class ProjectSecurityConfig {
                         return config;
                     }
                 }))
+                .addFilterBefore(new RequestValidationBeforeFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) ->
                         requests
-                                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user").authenticated()
+                                .requestMatchers("/myAccount").hasRole("USER")
+                                .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
+                                .requestMatchers("/myLoans").hasRole("USER")
+                                .requestMatchers("/myCards").hasRole("USER")
+                                .requestMatchers( "/user").authenticated()
                                 .requestMatchers("/notices", "/contact", "/register").permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
